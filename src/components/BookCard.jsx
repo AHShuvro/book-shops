39baf77f2f4bdd93778f8c19e391a3a4affb628c
Abul Hasan Shuvro <ahshuvro77777@gmail.com>
2024-10-19 01:38:25 -0700

    change some designs

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -9,9 +9,9 @@ const BookCard = ({ book, toggleWishlist, isWishlisted }) => {
             <img src={coverImageUrl} alt={book.title} className="w-full h-48 object-cover rounded-t-lg" />
             <div className="p-4">
                 <h2 className="font-bold text-lg md:text-2xl text-gray-800 hover:text-blue-600 transition-colors">{book.title}</h2>
-                <p className="text-gray-700 text-lg font-medium mt-1">Author: {book.authors.map(author => author.name).join(', ')}</p>
-                <p className="text-gray-500 mt-1">Genre: {book.subjects.join(', ')}</p>
-                <p className="text-gray-500 mt-1"><span className='font-semibold'>Id:</span> {book.id}</p>
+                <p className="text-gray-700 text-lg font-medium mt-1"><span className='font-semibold'>Author:</span> {book.authors.map(author => author.name).join(', ')}</p>
+                <p className="text-gray-500 mt-1"><span className='font-semibold text-gray-700'>Genre:</span> {book.subjects.join(', ')}</p>
+                <p className="text-gray-500 mt-1"><span className='font-semibold text-gray-700'>Id:</span> {book.id}</p>
                 <div className="flex justify-between items-center mt-4">
                     <Link to={`/book/${book.id}`} className="text-blue-500 font-medium ">View Details</Link>
                     <button 
@@ -22,7 +22,7 @@ const BookCard = ({ book, toggleWishlist, isWishlisted }) => {
                     </button>
                 </div>
             </div>
-        </div>
+        </div> 
     );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,11 +11,11 @@ function Navbar() {
     <div className="bg-white border-b border-gray-200 sticky top-0 left-0 right-0 z-20 shadow-md" tabIndex={0}>
             <div className="container mx-auto flex justify-between items-center p-4">
                 <Link to="/">
-                    <h1 className="text-2xl font-bold text-gray-800">Book Shop</h1>
+                    <h1 className="text-2xl font-bold text-gray-900">Book Shop</h1>
                 </Link>
                 <div className="hidden md:flex space-x-4">
-                    <Link to="/" className="text-2xl text-gray-700 hover:text-blue-500 transition">Home</Link>
-                    <Link to="/wishlist" className="text-2xl text-gray-700 hover:text-blue-500 transition">Wishlist</Link>
+                    <Link to="/" className="text-xl font-medium text-gray-900 hover:text-blue-500 transition">Home</Link>
+                    <Link to="/wishlist" className="text-xl font-medium text-gray-900 hover:text-blue-500 transition">Wishlist</Link>
                 </div>
                 <div className="md:hidden flex items-center">
                     <button 
